e9801629ab06058e1c8e05a9b3cd84217eda3333
LorddotHtml <114748042+LordDothtml@users.noreply.github.com>
2023-11-16 11:27:41 +0100

    Hold til AUTH_URL

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Login from './components/Login'
+import styled from "styled-components"
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Login />
+    </>
+  )
+}
+
+export default App
